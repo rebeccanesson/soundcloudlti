5f8c8248c1a0551f31db877858c0063137f7f400
Rebecca Nesson <rebeccanesson@gmail.com>
2014-12-09 12:17:57 -0500

    trying to add content to redirect url

--- app/controllers/soundcloud_embeds_controller.rb
+++ app/controllers/soundcloud_embeds_controller.rb
@@ -4,8 +4,8 @@ class SoundcloudEmbedsController < ApplicationController
     client = SoundCloud.new(:client_id => 'dd982ac2b116f6920ae53a60ae4d4c14')
     track_url = params[:url]
     track = client.get('/resolve', :url => track_url)
-    iframe_url = URI::escape("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/#{track.id}&color=0066cc")
-    logger.debug 'IFRAME URL : ' + iframe_url
+    iframe_url = Rack::Utils.escape("https://w.soundcloud.com/player/?url=https://api.soundcloud.com/tracks/#{track.id}&color=0066cc")
+    logger.info 'IFRAME URL : ' + iframe_url
     redirect_url = session[:launch_presentation_return_url] + '?return_type=iframe&url=' + iframe_url + '&width=100%&height=166'
     redirect_to redirect_url
   end
